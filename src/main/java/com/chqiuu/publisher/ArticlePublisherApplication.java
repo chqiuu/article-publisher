cb917b09bfb9f34edcadfea35cdfa7ffc3fc7a89
chqiuu <35096687+chqiuu@users.noreply.github.com>
2025-04-11 20:29:41 +0800

    项目初始化

---
+++ src/main/java/com/chqiuu/publisher/ArticlePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.chqiuu.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlePublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlePublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chqiuu/publisher/ArticlePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chqiuu.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlePublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
