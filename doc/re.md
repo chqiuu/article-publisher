488b5fa8bbe1b55d5b02f59fa9b34da4526a14f4
chqiuu <35096687+chqiuu@users.noreply.github.com>
2025-04-13 21:57:39 +0800

    处理页面跳转问题
    处理页面跳转问题

---
+++ doc/re.md
@@ -0,0 +1,5 @@
+
+![搜索联合传播样式-标准色版.png](https://p0-xtjj-private.juejin.cn/tos-cn-i-73owjymdk6/10c2046b3d114639b361c97361fb82ac~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56CB6KeJ5a6i:q75.awebp?policy=eyJ2bSI6MywidWlkIjoiMzExMjA4ODc5NDMxMTU0MCJ9&rk3s=f64ab15b&x-orig-authkey=f32326d3454f2ac7e96d3d06cdbb035152127018&x-orig-expires=1745066686&x-orig-sign=JJ9JLzuRml7ioPLhK2po0AZK4BA%3D)
+
+
+
---
+++ src/main/resources/static/res/json/menu.js
@@ -0,0 +1,34 @@
+{
+  "code": 0,
+  "msg": "",
+  "data": [{
+    "name": "home",
+    "title": "主页",
+    "icon": "layui-icon-home",
+    "children": [{
+      "title": "控制台",
+      "url": "/"
+    }]
+  }, {
+    "name": "article",
+    "title": "文章管理",
+    "icon": "layui-icon-template",
+    "children": [{
+      "title": "文章列表",
+      "url": "/article/ArticlesTable"
+    }]
+  }, {
+    "name": "set",
+    "title": "设置",
+    "icon": "layui-icon-set",
+    "children": [{
+      "title": "平台管理",
+      "url": "/platform/PlatformsTable"
+    }]
+  }, {
+    "name": "about",
+    "title": "关于",
+    "icon": "layui-icon-auz",
+    "url": "/system/about"
+  }]
+}
